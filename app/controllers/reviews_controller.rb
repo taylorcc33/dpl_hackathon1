82eb05835844de7c8fa168a8a25de6055a9f69c2
Joshua Parco <jacparco@gmail.com>
2020-12-05 11:51:35 -0700

    set up new and create in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_trip
+
   def index
   end
 
@@ -6,8 +8,28 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = @trip.reviews.new(review_params)
+    if @review.save
+      redirect_to user_trip_path(@trip.user_id, @trip)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :body, :score)
   end
 end
