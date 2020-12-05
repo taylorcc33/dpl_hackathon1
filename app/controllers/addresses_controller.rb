b3ac81185bed3a012f8acffc62443471addfd105
dani7beth <danielledona28@gmail.com>
2020-12-05 11:50:19 -0700

    added new and create methods in controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,34 @@
 class AddressesController < ApplicationController
+  before_action :get_location, only: [:new, :create]
+
   def index
+    @addresses = Adresss.all
   end
 
   def show
   end
 
   def new
+    @address = Address.new
+  end
+
+  def create
+    @address = @location.addresses.new(address_params)
+    if @address.save
+      redirect_to @location
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+  def get_location
+    @location = Location.find(params[:location_id])
+  end
+  def address_params
+    params.require(:address).permit(:street, :city, :state, :zip, :country, :location_id)
   end
 end
