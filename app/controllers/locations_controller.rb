1ec0eb18de59d1cd8c9dd8d694108f383b68449b
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-05 11:59:09 -0700

    made locations index method and updated the view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,25 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+
   def index
+      # sign in with user, then go to a trip, then you'll see the locations. So I can currect_user.trips.locations.all
+      @locations = @trip.locations
   end
 
   def show
+    @location = @trip.location.find(params[:id])
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+    ## or @trip = current_user.trip(params[:id])
   end
 end
