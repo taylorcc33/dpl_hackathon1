12fccf78facb5abb0b670d2b51f0728a3fb52294
Taylor Collins <taylorcc33@gmail.com>
2020-12-05 13:56:04 -0700

    updated seeds.rb with all object types

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,9 @@ User.create(email:'user2@test.com', first_name:'John', last_name:'Doe', password
 Trip.create(name:'Trip 1', description:'Trip 1 description', user_id:1)
 Trip.create(name:'Trip 2', description:'Trip 2 description', user_id:1)
 
-Location.create(name:'Location test', description:'location description testing', type:'hotel', trip_id:1)
+Location.create(name:'Location test', description:'location description testing', kind:'hotel', trip_id:1)
+Location.create(name:'Location test2', description:'location description testing2', kind:'restaurant', trip_id:1)
+
+Address.create(street:"Random ave", city:"New York", state:"NY", zip:"83920", country:"USA",location_id:1)
+
+Review.create(title:"Review1", body:"this is a review 1", score:3, trip_id:1)
