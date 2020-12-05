c436959093c281bd327bf21dca625dab1c01bd32
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-05 12:24:33 -0700

    made location show method and updated the view.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
   before_action :set_trip
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   def index
       # sign in with user, then go to a trip, then you'll see the locations. So I can currect_user.trips.locations.all
@@ -7,13 +8,36 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = @trip.location.find(params[:id])
   end
 
   def new
+    @location = @trip.location.new
+  end
+
+  def create
+    @location = @trip.location.new(location_params)
+
+      if @location.save
+        redirect_to @locations
+      else 
+        render :new
+      end
   end
 
   def edit
+  end
+
+  def update
+    if @location.update(location_params)
+      redirect_to trip_path(@trip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to @trip
   end
 
   private
@@ -21,5 +45,13 @@ class LocationsController < ApplicationController
   def set_trip
     @trip = Trip.find(params[:trip_id])
     ## or @trip = current_user.trip(params[:id])
+  end
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :description, :type)
   end
 end
