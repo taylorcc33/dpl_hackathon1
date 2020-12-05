248e09c0f454d2cb108599089e406f1556bf9db1
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-05 16:12:25 -0700

    merging, added views and stuff

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,4 @@
 class Trip < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
   has_many :locations, dependent: :destroy
-=======
-  has_many :locations , dependent: :delete_all
->>>>>>> 2217d603f7ff030f6ed07d458a5fae7a73f7d126
 end
